3552896e08b31540ef95227e5efcc005cc0cfca5
slomalos <113264825+slomalos@users.noreply.github.com>
2026-04-24 09:36:38 +0300

    Add files via upload

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ollama serve &
+
+echo "Waiting for Ollama to start..."
+sleep 5
+
+echo "Downloading Qwen2.5:0.5b model..."
+ollama pull qwen2.5:0.5b
+
+echo "Starting FastAPI server..."
+uvicorn app.main:app --host 0.0.0.0 --port 8000
